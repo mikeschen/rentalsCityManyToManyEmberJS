8e8c7193cc13c5e6eb2b91195ebf11b42166e9d6
Michael Schennum and Polina Nenchev <mikeschenphoto@gmail.com>
2016-03-29 09:22:33 -0700

    Add rental update component and route

--- app/components/rental-tile.js
+++ app/components/rental-tile.js
@@ -4,10 +4,13 @@ export default Ember.Component.extend({
 	isImageShowing: false,
 	actions: {
 		imageShow: function() {
-			this.set('isImageShowing', true); 
+			this.set('isImageShowing', true);
 		},
 		imageHide: function() {
 			this.set('isImageShowing', false);
+		},
+		update(rental, params) {
+			this.sendAction('update', rental, params);
 		},
 		delete(rental) {
 			if (confirm('Are you sure you want to delete this rental?')) {
